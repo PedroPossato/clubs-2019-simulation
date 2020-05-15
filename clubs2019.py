29ee608c3b5ced4fa0f86abc843afc6df5a7d646
PedroPossato <pedro.possato@gmail.com>
2020-05-15 15:53:28 -0300

    Simulations working properly & added feature
    Added feature: Now you can see the average result (points) of the tournament after your simulations.

--- clubs2019.py
+++ clubs2019.py
@@ -67,6 +67,7 @@ times =         ['FLAMENGO',    'SANTOS',   'PALMEIRAS',    'GREMIO',   'ATHLETI
 power =         [2.32432,       1.81818,    1.90625,        1.64103,    1.59375,        1.3,            1.12821,            1.2353,        1.02041,     0.71875,    1.02326,    0.86667,    0.91837,        0.82609,        0.68889,    0.87805,    0.58696,    0.41379,    0.59615,        0.29032,    3.96971,       2.77065,                2.61640,        1.65173,        1.85253,                1.52314,            1.51571,            1.48413,        1.40343,    1.07363,    1.02626,            1.01596,    0.77018,        0.85016,        1.01047,    0.88416,    0.77508,    0.77935,        0.76688,        0.60725,     3.01072,            2.20961,    2.55705,    1.75144,            1.60847,            0.98295,        1.21528,        1.04167,    0.87281,        0.92934,    0.83692,        0.99385,                0.71487,            0.74237,    0.68790,    0.57905,        0.52641,            0.59573         ]
 offensiveness = [2.20361,       1.37119,    1.3518,         1.72853,    1.13019,        0.81025,        1.18837,            0.98892,       1.69668,     2.03878,    1.31025,    1.21537,    1.52701,        1.21053,        0.96607,    1.02216,    0.86011,    0.96399,    1.11634,        0.77285,    1.64804,       2.68878,                1.93103,        2.36504,        1.56956,                1.65755,            0.95663,            2.23543,        1.83673,    1.61712,    0.98930,            2.02378,    1.21879,        2.16409,        1.52200,    2.08086,    1.41260,    1.62069,        2.42857,        1.54578,     3.03680,            3.59040,    2.57920,    2.35200,            2.16000,            1.90080,        1.63200,        1.90400,    2.40800,        2.80800,    2.09920,        2.70486,                2.45760,            2.99520,    2.88320,    2.16000,        2.57813,            2.59200         ]
 vogal =         ['o',           'o',        'o',            'o',        'o',            'o',            'o',                'o',            'o',        'o',            'o',    'o',            'o',        'o',            'o',        'o',        'o',        'o',        'a',            'o',        'o',           'o',                    'o',            'o',            'o',                    'o',                'o',                'o',            'o',        'o',        'o',                'o',        'o',            'o',            'o',        'o',        'o',        'o',            'o',            'o',         'o',                'o',        'o',        'o',                'o',                'o',            'o',            'o',        'o',            'o',        'o',            'o',                    'o',                'o',        'o',        'o',            'o',                'o'             ]
+mediaPontos = []
 atk = []
 defense = []
 fieldFactor =   []
@@ -83,7 +84,7 @@ for i in range(len(times)):
 for i in range(len(times)):
     atk.append( sqrt( offensiveness[i] * power[i] ) )
     defense.append( sqrt( offensiveness[i]/(power[i]*1.0) ) )
-mediaGolsBrasileirao    =  3.300 # Por meio de simulacoes, com esse valor chegamos em 2.305 gols por partida em media ¯\_(ツ)_/¯
+mediaGolsBrasileirao    =  3.297 # Por meio de simulacoes, com esse valor chegamos em 2.305 gols por partida em media ¯\_(ツ)_/¯
 mediaGolsPremierLeague  =  2.774 # Por meio de simulacoes, com esse valor chegamos em 2.722 gols por partida em media ¯\_(ツ)_/¯
 mediaGolsBundesliga     =  2.340 # Por meio de simulacoes, com esse valor chegamos em 3.250 gols por partida em media ¯\_(ツ)_/¯
 atkMedioBrasileirao = 2 * sum(atk)/len(atk) * sum(atk)/len(atk) / mediaGolsBrasileirao
@@ -125,6 +126,9 @@ elif a%((numModos+1)/2) == 2:
     atk =           atk[40:]
     defense =       defense[40:]
 
+for i in times:
+    mediaPontos.append(0)
+
 print()
 aux = times[:]
 auxAtk = atk[:]
@@ -152,10 +156,6 @@ for letra in vogal:
 vezesCampeao = []
 for num in times:
     vezesCampeao.append(0)
-
-pontos = []
-for i in times:
-    pontos.append(0)
 
 while True:
     if lightSpeed:
@@ -200,7 +200,7 @@ def findLambda(A, B, neutrality, extraTime = False):
         lambdaA = lambdaA / 3.0
         lambdaB = lambdaB / 3.0
     else:
-        if not lightSpeed:
+        if not lightSpeed and not gameMode:
             print("Resultado esperado: {} {:.2f} vs {:.2f} {}\n".format(A, lambdaA, lambdaB, B))
     return lambdaA, lambdaB
 
@@ -225,8 +225,8 @@ def poisson(A, B, neutrality, extraTime = False):
     golsB = draftGoal(P2)
     return golsA, golsB
 
-def match(A, B, grupo, neutrality, amistoso, knockOut = False):
-    if not lightSpeed:
+def match(pontos, mediaPontos, A, B, grupo, neutrality, amistoso, lastGame = True, knockOut = False):
+    if not lightSpeed and not gameMode:
         print("\n========================================================================================================================\n")
         print("\nPARTIDA:", A.upper(), "x", B.upper())
         print()
@@ -256,11 +256,11 @@ def match(A, B, grupo, neutrality, amistoso, knockOut = False):
                     placarB += 1
                 else:
                     placarA += 1
-                if not lightSpeed:
+                if not gameMode:
                     goalEvent(A, B, sideScoring[count], placarA, placarB, tempo)
                 count += 1
             else:
-                if not lightSpeed:
+                if not gameMode:
                     otherEvents(A, B, tempo)
             if not gameMode:
                 sleep(0.1)
@@ -283,8 +283,8 @@ def match(A, B, grupo, neutrality, amistoso, knockOut = False):
 
     if not lightSpeed:
         print("FINAL DE PARTIDA:",A.upper(),golsA,'x',golsB,B.upper())
-    if not knockOut and not amistoso:
-        showTable(grupo)
+    if not knockOut and not amistoso and lastGame:
+        showTable(pontos, mediaPontos, grupo)
     if not gameMode:
         input("\nPartida terminada. Pressione ENTER para continuar...")
     if not lightSpeed:
@@ -335,7 +335,7 @@ def goalEvent(A, B, side, placarA, placarB, tempo):
     print(A.upper(), placarA, "x", placarB, B.upper())
     print()
 
-def showTable(grupo, fim = False):
+def showTable(pontos, mediaPontos, grupo, fim = False):
     try:
         timesGrupo = times[:]
         pontosGrupo = pontos[:]
@@ -349,11 +349,15 @@ def showTable(grupo, fim = False):
             if not lightSpeed:
                 print(timesGrupo[0],"EH {} CAMPEA{}!".format(vogal[index].upper(), gramaticaCampeao[index].upper()))
             else:
+                #for i in range(len(grupo)):
+                    #print("{}:\t{:.0f} pts ->\t{}".format(i+1,pontosGrupo[i],timesGrupo[i]))
                 vezesCampeao[index] += 1
+                for ponto in range(len(times)):
+                    mediaPontos[ponto] += pontos[ponto]
     except Exception as e:
         print(e)
 
-def playGrupo(grupo):
+def playGrupo(grupo, pontos):
     for turno in range(2):
         teamsNum = len(times)
         teamHome = 0
@@ -369,21 +373,25 @@ def playGrupo(grupo):
             if not lightSpeed:
                 print("\nRodada {}:".format(i+1+19*turno))
             for j in range(int(len(times)/2)):
+                if j == int(len(times)/2)-1:
+                    lastGame = True
+                else:
+                    lastGame = False
                 if not j:
                     x = 0
                 else:
                     x = teamHome
                 if jogosCasa[x] <= jogosCasa[teamAway]:
                     if turno:
-                        match(times[teamAway], times[x], grupo, False, False)
+                        match(pontos, mediaPontos, times[teamAway], times[x], grupo, False, False, lastGame)
                     else:
-                        match(times[x], times[teamAway], grupo, False, False)
+                        match(pontos, mediaPontos, times[x], times[teamAway], grupo, False, False, lastGame)
                     jogosCasa[x] += 1
                 else:
                     if turno:
-                        match(times[x], times[teamAway], grupo, False, False)
+                        match(pontos, mediaPontos, times[x], times[teamAway], grupo, False, False, lastGame)
                     else:
-                        match(times[teamAway], times[x], grupo, False, False)
+                        match(pontos, mediaPontos, times[teamAway], times[x], grupo, False, False, lastGame)
                     jogosCasa[teamAway] += 1
                 teamHome += 1
                 if teamHome > teamsNum - 1:
@@ -423,9 +431,9 @@ def main():
             if not lightSpeed:
                 print("Comeca o campeonato!\n")
 
-            playGrupo(times)
+            playGrupo(times, pontos)
 
-            showTable(times, True)
+            showTable(pontos, mediaPontos, times, True)
         else:
             while True:
                 while True:
@@ -454,12 +462,14 @@ def main():
                         break
                     else:
                         print("Valor invalido! Tente novamente...")
-                match(time1, time2, times, neutro, True)
+                match(pontos, mediaPontos, time1, time2, times, neutro, True)
 
     if lightSpeed and not amistoso:
-        #aux = vezesCampeao[:]
+        aux = times[:]
+        for i in range(len(times)):
+            mediaPontos[i] /= (numSimuls*1.0)
         order_map(times, vezesCampeao, 1)
-        #order_map(meta, aux, 1)
+        order_map(aux, mediaPontos, 1)
         print("Numero de Simulacoes realizadas: {}\nTempo de execucao: {:.2f} segundos".format(numSimuls, time() - inicio))
         print("Media de gols por jogo: {:.3f}".format(gols/(jogos*1.0)))
         for i in range(len(times)):
@@ -468,6 +478,8 @@ def main():
             #else:
             debugMessage = ''
             print("{}:\t{}x campeao ->\t{} {}".format(i+1, vezesCampeao[i], times[i], debugMessage))
-        
+        print()
+        for i in range(len(times)):
+            print("{}:\t{:.0f} pts ->\t{}".format(i+1, mediaPontos[i], aux[i]))
 if __name__ == '__main__':
     main()
